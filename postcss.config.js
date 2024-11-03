7b05fa7f4ea69abd5d5fa02ec3f377ce2ae6ef4b
NXGEN <fork@nxgen.tech>
2024-11-03 16:04:37 -0500

    Update GitHub Actions workflow with PostCSS support

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+    plugins: {
+        autoprefixer: {
+            browsersList: [
+                "last 2 versions",
+                "> 1%",
+                "maintained node versions"
+            ]
+        }
+    }
+}
